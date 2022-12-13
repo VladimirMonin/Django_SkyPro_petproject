ef1f10ba97a2df198759af40ceb7cb27ce17bb60
v <79788260286@yandex.ru>
2022-12-13 12:52:08 +0600

    add: view company and urls settings

--- companies/urls.py
+++ companies/urls.py
@@ -6,7 +6,7 @@ from companies import views
 # В такой интерпретации urls (когда они лежат внутри каждого app - оставляем сам путь без префикса)
 urlpatterns = [
 
-    path('image/<int:pk>', views.CompanyImageView.as_view()),
+    path('image/<int:pk>/', views.CompanyImageView.as_view()),
     # специальный метод as.view() делает вызываемым класс (с передачей реквеста внутрь и упрощая задачу)
 
 ]
