39b9c746ddeaf3741e7565bdf09aff0e9515ac77
v <79788260286@yandex.ru>
2022-12-13 12:17:40 +0600

    media: add media in settings.py and urls.py

--- Django_Skypro_petprodject/urls.py
+++ Django_Skypro_petprodject/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),  # comments: Показали что будет работать по этому адресу
     path('vacancy/', include('vacancies.urls')),  # добавили урлы из app vacancies
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # добавили сам урл (с константы настроек а потом адрес папки тоже константой)
