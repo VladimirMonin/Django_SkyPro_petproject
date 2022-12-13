7ae84b0df24053737b6f0f24c39eb710ebe99b35
v <79788260286@yandex.ru>
2022-12-13 12:56:22 +0600

    feat: comments

--- companies/views.py
+++ companies/views.py
@@ -14,11 +14,11 @@ class CompanyImageView(UpdateView):
 
     def post(self, *args, **kwargs):
         self.object = self.get_object()
-        self.object.logo = request.FILES['logo']
+        self.object.logo = request.FILES['logo']   # FILES лежат все файлы которые послал пользователь
         self.object.save()
 
         return JsonResponse({
             'id': self.object.id,
             'name': self.object.name,
-            'logo': self.object.logo.url if self.object.logo else None,
+            'logo': self.object.logo.url if self.object.logo else None,  # Чтобы не было проблем с отображением - показываем урл - если его нет поставим NONE
         })
