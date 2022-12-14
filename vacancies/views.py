073150c00fc942d003f0a7b20af4f4550c1c4083
v <79788260286@yandex.ru>
2022-12-14 14:55:49 +0600

    feat: Avg count in UserVacancyDetailView

--- vacancies/views.py
+++ vacancies/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from django.db.models import Count
+from django.db.models import Count, Avg
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
@@ -156,7 +156,7 @@ class VacancyDeleteView(DeleteView):
 @method_decorator(csrf_exempt, name='dispatch')
 class UserVacancyDetailView(View):
     def get(self, request):
-        user_qs = User.objects.annotate(vacancies=Count('vacancy')) # Этот метод добавляет к нашей записи доп. колонку в которую ложет данные что он сделал - например это значение функции (посчтитать, макс или мин)
+        user_qs = User.objects.annotate(vacancies=Count('vacancy')).order_by('id') # Этот метод добавляет к нашей записи доп. колонку в которую ложет данные что он сделал - например это значение функции (посчтитать, макс или мин) далее я делаю группировку по возрастанию ID
 
         paginator = Paginator(user_qs, settings.TOTAL_ON_PAGE)  # Пагинатор встроен в Джанго.
         page_number = request.GET.get('page')  # Достаём номер страницы из запроса
@@ -173,8 +173,9 @@ class UserVacancyDetailView(View):
 
         response = {
             'items': users,
-            'num_pages': paginator.num_pages,  # Посчитаем сколько всего страниц
-            'total': paginator.count  # Посчитаем сколько всего записей
+            'num_pages': paginator.num_pages,  # Посчитаем сколько всего страниц у нас будет
+            'total': paginator.count,  # Посчитаем сколько всего юзеров
+            'avg': user_qs.aggregate(avg=Avg('vacancies'))['avg']  # aggregate - терминальная функция, после неё уже нельзя использовать гурппировки. Тут мы говорим сделай Avg, положи в ключ avg и последний список после скобок (чтобы было плоско)
         }
 
         return JsonResponse(response, safe=False, json_dumps_params={
