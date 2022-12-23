f394dd39dec1cf6196dce7d504628f218da390a7
VladimirMonin <79788260286@yandex.ru>
2022-12-23 17:06:18 +0600

    feat: add SlugRelatedField in VacancySerializer for ListView

--- vacancies/serializers.py
+++ vacancies/serializers.py
@@ -4,15 +4,23 @@ from vacancies.models import Vacancy
 
 
 class VacancySerializer(serializers.ModelSerializer):
+    #  тут мы можем как добавить новое поле, так и переопределить поле модели
     username = serializers.CharField()  # username в модели нет - но мы можем добавить в сериализатор (а данные добавляются во вьюшке)
+    skills = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'  # На какое поле модели skills мы будем ссылаться
+    )
 
     class Meta:
         model = Vacancy  # Модель которую будем прогонять
         fields = ['id', 'text', 'slug', 'status', 'created',
-                  'username']  # Поля (тут можно сделать исключения, или просто указать __all__
+                  'username', 'skills']  # Поля (тут можно сделать исключения, или просто указать __all__
 
 
 class VacancyDetailSerializer(serializers.ModelSerializer):
+    user = serializers.CharField()  # если добавить сюда эту строку - будет подтягиваться Юзернейм!!!
+
     class Meta:
         model = Vacancy  # Модель которую будем прогонять
         fields = '__all__'
