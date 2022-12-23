2779ee4150ca6accd3042a2692c496b3def5a3a2
VladimirMonin <79788260286@yandex.ru>
2022-12-23 15:27:01 +0600

    feat: add serializers.py and VacancyDerializer

--- Django_Skypro_petprodject/urls.py
+++ Django_Skypro_petprodject/urls.py
@@ -7,7 +7,7 @@ from vacancies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls')),  # Добавили урл из DRF - для проверки. В нём есть собственный urls.py - и пути для авторизации
+    path('api-auth/', include('rest_framework.urls')),  # Добавили урл из DRF - для проверки. В нём есть собственный urls.py - и пути для авторизации
     path('hello/', views.hello),  # comments: Показали что будет работать по этому адресу
     path('vacancy/', include('vacancies.urls')),  # добавили урлы из app vacancies
     path('company/', include('companies.urls')),
@@ -15,3 +15,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # добавили сам урл (с константы настроек а потом адрес папки тоже константой) работает только в режиме разработчика и для этого нужна проверка
+
