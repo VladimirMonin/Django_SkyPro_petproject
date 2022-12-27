92f0da8487de4bb0aae3453dc8607eb36a370c84
VladimirMonin <79788260286@yandex.ru>
2022-12-27 18:01:40 +0600

    fix: name of model Profile

--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Prifile(models.Model): # Создали доп. таблицу. Ссылкается на пользователя. На 1 запись пользователя - 1 профиль
+class Profile(models.Model): # Создали доп. таблицу. Ссылкается на пользователя. На 1 запись пользователя - 1 профиль
     MALE = 'm'
     FEMALE = 'f'
     SEX = [(MALE, 'Male'), (FEMALE, 'Female')]
