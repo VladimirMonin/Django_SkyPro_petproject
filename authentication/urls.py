2ab721d0a611e33aee1fdd6048a958c3418353bb
VladimirMonin <79788260286@yandex.ru>
2022-12-30 22:51:40 +0600

    feat: user/logout/ is work!

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
-from authentication.views import UserCreateView
+from authentication.views import UserCreateView, UserLogout
 
 urlpatterns = [
     path('create/', UserCreateView.as_view()),
-    path('login/', views.obtain_auth_token)  # вьюшка которая нам даст auth_token (написана функцией)
+    path('login/', views.obtain_auth_token),  # вьюшка которая нам даст auth_token (написана функцией)
+    path('logout/', UserLogout.as_view()),
 
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,7 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from authentication.models import User
 from authentication.serializers import UserCreateSerializer
@@ -7,3 +10,9 @@ from authentication.serializers import UserCreateSerializer
 class UserCreateView(CreateAPIView):
     model = User
     serializer_class = UserCreateSerializer
+
+
+class UserLogout(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
