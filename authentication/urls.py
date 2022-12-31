c7a55000f05191830ec05d88087b3361239e30d6
VladimirMonin <79788260286@yandex.ru>
2022-12-31 12:54:44 +0600

    feat: add urls token and token/refresh for JWT auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from authentication.views import UserCreateView, UserLogout
 
@@ -7,5 +8,7 @@ urlpatterns = [
     path('create/', UserCreateView.as_view()),
     path('login/', views.obtain_auth_token),  # вьюшка которая нам даст auth_token (написана функцией)
     path('logout/', UserLogout.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 
 ]
