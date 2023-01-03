ce29b986ab42fc0a0df194c4f49dccfcfff2fe9c
VladimirMonin <79788260286@yandex.ru>
2023-01-03 15:30:07 +0600

    feat: UserVacancyDetailView to user_vacancies - func method in DRM and permission

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -12,11 +12,12 @@ router.register(r'skill', views.SkillsViewSet)
 urlpatterns = [
 
     path('', include(router.urls)),  # Подключаем роут
-    path('', views.VacancyListView.as_view()),  # специальный метод as.view() делает вызываемым класс (с передачей реквеста внутрь и упрощая задачу)
+    path('', views.VacancyListView.as_view()),  # специальный метод as.view() делает вызываемым класс (с передачей
+    # реквеста внутрь и упрощая задачу)
     path('detail/<int:pk>/', views.VacancyDetailView.as_view()),  # тут передается либо pk либо slug
     path('create/', views.VacancyCreateView.as_view()),
     path('update/<int:pk>/', views.VacancyUpdateView.as_view()),
-    path('by_users/', views.UserVacancyDetailView.as_view()),
+    path('by_users/', views.user_vacancies),
     path('delete/<int:pk>/', views.VacancyDeleteView.as_view()),
     path('like/', views.VacancyLikeView.as_view()),
 
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.contrib.auth.models import User
+
 from django.core.paginator import Paginator
 from django.db.models import Count, Avg, Q, F
 from django.http import HttpResponse, JsonResponse
@@ -10,11 +10,13 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from Django_Skypro_petprodject import settings
+from authentication.models import User
 from vacancies.models import Vacancy, Skill
 from vacancies.permissions import VacancyCreatePermission
 from vacancies.serializers import VacancySerializer, VacancyDetailSerializer, VacancyCreateSerializer, \
@@ -81,35 +83,36 @@ class VacancyDeleteView(DestroyAPIView):
     serializer_class = VacancyDestroySerializer
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class UserVacancyDetailView(View):
-    def get(self, request):
-        user_qs = User.objects.annotate(vacancies=Count('vacancy')).order_by(
-            'id')  # Этот метод добавляет к нашей записи доп. колонку в которую ложет данные что он сделал - например это значение функции (посчтитать, макс или мин) далее я делаю группировку по возрастанию ID
+# ЕСЛИ МЫ ХОТИМ ПИСАТЬ  API НА ФУНКЦИЯХ
+@api_view(['GET'])  # Говорим DRM что это вьюшка с методом GET
+@permission_classes([IsAuthenticated])  # Передаем разрешения на доступ
+def user_vacancies(request):
+    user_qs = User.objects.annotate(vacancies=Count('vacancy')).order_by(
+        'id')  # Этот метод добавляет к нашей записи доп. колонку в которую ложет данные что он сделал - например это значение функции (посчтитать, макс или мин) далее я делаю группировку по возрастанию ID
 
-        paginator = Paginator(user_qs, settings.TOTAL_ON_PAGE)  # Пагинатор встроен в Джанго.
-        page_number = request.GET.get('page')  # Достаём номер страницы из запроса
-        page_object = paginator.get_page(page_number)  # Передаем в пагинатор и получаем страницу
+    paginator = Paginator(user_qs, settings.TOTAL_ON_PAGE)  # Пагинатор встроен в Джанго.
+    page_number = request.GET.get('page')  # Достаём номер страницы из запроса
+    page_object = paginator.get_page(page_number)  # Передаем в пагинатор и получаем страницу
 
-        users = []
+    users = []
 
-        for user in page_object:
-            users.append({
-                'id': user.id,
-                'user': user.username,
-                'vacancies': user.vacancies  # Как раз из юзер квери сета - который мы посчитали в переменной user_qs
-            })
+    for user in page_object:
+        users.append({
+            'id': user.id,
+            'user': user.username,
+            'vacancies': user.vacancies  # Как раз из юзер квери сета - который мы посчитали в переменной user_qs
+        })
 
-        response = {
-            'items': users,
-            'num_pages': paginator.num_pages,  # Посчитаем сколько всего страниц у нас будет
-            'total': paginator.count,  # Посчитаем сколько всего юзеров
-            'avg': user_qs.aggregate(avg=Avg('vacancies'))['avg']
-            # aggregate - терминальная функция, после неё уже нельзя использовать гурппировки. Тут мы говорим сделай Avg, положи в ключ avg и последний список после скобок (чтобы было плоско)
-        }
+    response = {
+        'items': users,
+        'num_pages': paginator.num_pages,  # Посчитаем сколько всего страниц у нас будет
+        'total': paginator.count,  # Посчитаем сколько всего юзеров
+        'avg': user_qs.aggregate(avg=Avg('vacancies'))['avg']
+        # aggregate - терминальная функция, после неё уже нельзя использовать гурппировки. Тут мы говорим сделай Avg, положи в ключ avg и последний список после скобок (чтобы было плоско)
+    }
 
-        return JsonResponse(response, safe=False, json_dumps_params={
-            'ensure_ascii': False})
+    return JsonResponse(response, safe=False, json_dumps_params={
+        'ensure_ascii': False})
 
 
 class VacancyLikeView(UpdateAPIView):
@@ -126,5 +129,5 @@ class VacancyLikeView(UpdateAPIView):
         return JsonResponse(
             VacancyDetailSerializer(Vacancy.objects.filter(pk__in=request.data),
                                     many=True).data,
-                                    safe=False,
+            safe=False,
         )
