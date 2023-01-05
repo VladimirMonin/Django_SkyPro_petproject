ab9dcc7c6f9360653e60d6d89b41919fb99245ed
VladimirMonin <79788260286@yandex.ru>
2023-01-05 11:37:05 +0600

    test: add test_create_vacancy with fixtures.py and conftest.py

---
+++ tests/fixtures.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def hr_token(client, django_user_model):
+    username = 'hr'
+    password = '123qwe'
+
+    django_user_model.objects.create_user(
+        username=username,
+        password=password,
+        role='hr'
+    )
+
+    response = client.post(
+        "/user/login/",
+        {"username": username, "password": password},
+        format='json'  # альтернатива content_type="application/json"
+    )
+
+    return response.data['token']
---
+++ tests/vacancies/vacancy_create_test.py
@@ -0,0 +1,35 @@
+# Create работает по позитивному сценарию. Потом проверка всех валидаторов
+from datetime import date
+import pytest
+
+
+@pytest.mark.django_db
+def test_create_vacancy(client, hr_token):
+    expected_response = {
+        "id": 2,
+        "created": date.today().strftime('%Y-%m-%d'),
+        "skills": [],
+        "slug": "123",
+        "text": "123",
+        "status": "draft",
+        "min_experience": None,
+        "likes": 0,
+        "updated_at": None,
+        "user": None
+    }
+
+    data = {
+        "slug": "123",
+        "text": "123",
+        "status": "draft",
+    }
+
+    response = client.post(
+        "/vacancy/create/",
+        data,
+        content_type="application/json",
+        HTTP_AUTHORIZATION = "Token " + hr_token
+    )
+
+    assert response.status_code == 201
+    assert response.data == expected_response
